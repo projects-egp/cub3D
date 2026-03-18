0cd430af2ae928680c3b5ec929ebe5315819a1a4
Enrique Gil Piqueras <enrgil-p@c3r12s6.42madrid.com>
2026-03-18 17:45:18 +0100

    Structure

--- include/general.h
+++ include/general.h
@@ -6,8 +6,16 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 17:32:53 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/03/18 17:32:55 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/03/18 17:44:25 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GENERAL_H
+# define GENERAL_H
 
+# include "mlx_linx/mlx.h"
+# include "mlx_linx/mlx_int.h"
+//Insert here new headers
+# include "definitions.h"
+
+#endif
--- include/parse.h
+++ include/parse.h
@@ -1,25 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parse.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/15 18:23:38 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/03/18 17:38:51 by enrgil-p         ###   ########.fr       */
+/*   Created: 2026/03/18 17:41:10 by enrgil-p          #+#    #+#             */
+/*   Updated: 2026/03/18 17:41:26 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argv)
-{
-	t_mlx	mlx_data;//t_mlx must be defined, is not given by minilibx
-
-	//Parse map
-	if (argc == 2)
-	{
-		if (!open_map(argv[1], &mlx_data)
-		//Display window
-		return (0);
-	}
-	return (1);
-}
+#include "general.h"
