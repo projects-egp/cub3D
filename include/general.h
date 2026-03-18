9c2fa43cbc0bdaf129194949c0fc3844c119930c
Enrique Gil Piqueras <enrgil-p@c3r12s6.42madrid.com>
2026-03-18 20:40:32 +0100

    Headers modified to open files and store data from it

--- include/general.h
+++ include/general.h
@@ -6,13 +6,14 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 17:32:53 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/03/18 19:27:30 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/03/18 20:22:09 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GENERAL_H
 # define GENERAL_H
 
+# include <fcntl.h>
 # include "../mlx_linux/mlx.h"
 # include "../mlx_linux/mlx_int.h"
 # include "../libft/complete_libft.h"
