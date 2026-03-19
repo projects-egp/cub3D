096b3d2f58b6bed4a6520924a0aa3ddb7bb84248
Enrique Gil Piqueras <enrgil-p@c3r13s6.42madrid.com>
2026-03-19 17:35:42 +0100

    Included read_data() prototype

--- include/parse.h
+++ include/parse.h
@@ -6,10 +6,11 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 17:41:10 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/03/18 18:16:22 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/03/19 17:35:10 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "general.h"
 
-int	open_map(char *cub_file_path, t_mlx *data);
+int	open_file(char *cub_file_path, t_mlx *data);
+int	read_data(char *cub_file_path, t_mlx *data, int fd);
