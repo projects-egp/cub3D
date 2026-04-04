2408970d8665c8f116b7cffadd9cd84748f38763
Enrique Gil Piqueras <enrgil-p@c2r8s1.42madrid.com>
2026-04-04 18:07:42 +0200

    Now in succesful case of open_file(), just return 1. And init mlx_data will be called in main(). This is the way to do without leaks and avoiding mallocs for structs

--- include/parse.h
+++ include/parse.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 17:41:10 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/03/22 18:56:00 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/04/04 17:51:29 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "general.h"
 
-int	open_file(char *cub_file_path, t_mlx *data);
+int	open_file(char *cub_file_path, t_map *file_data);
 int	read_data(t_map *data, int fd);
 int	add_scene_data(char *line, t_map *data);
 int	is_empty_line(char *line, t_map *data);
--- sources/main.c
+++ sources/main.c
@@ -6,23 +6,31 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 18:23:38 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/04/01 21:59:56 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/04/04 17:57:54 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "general.h"
 
+static void	init_mlx_data(t_mlx *mlx_data, t_map *file_data)
+{
+	mlx_data->map_data = file_data;
+	return ;
+}
+
 int	main(int argc, char **argv)
 {
 	t_mlx	mlx_data;//t_mlx must be defined, is not given by minilibx
+	t_map	file_data;
 	
 	if (argc != 2)
 	{
 		ft_putendl_error("Error\nExpected usage: ./cub3D [filename].cub");
 		return (1);
 	}
-	if (!open_file(argv[1], &mlx_data))
+	if (!open_file(argv[1], &file_data))
 		return (1);
+	init_mlx_data(&mlx_data, &file_data);
 	clean_up(&mlx_data);
 	return (0);
 }
