2e0314b1ad28d1154718b4daa033aaa214d3f720
Enrique Gil Piqueras <enrgil-p@c2r10s2.42madrid.com>
2026-05-08 16:47:04 +0200

    add_scene_data.c. Now is checked in valid_identifier() if current identifier has been already found

--- sources/parse/add_scene_data.c
+++ sources/parse/add_scene_data.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 19:04:46 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/04/17 15:02:37 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/05/08 16:44:37 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,18 @@ static int	error_found(char **array)
 
 static int	valid_identifier(char *identifier)
 {
-	if ((identifier[0] == 'F' || identifier[0] == 'C') && identifier[1] == 0)
+	if ((identifier[0] == 'F' || identifier[0] == 'C')
+		&& identifier[1] == 0 && !already_found(identifier[0]))
 		return (1);
 	else if ((identifier[0] == 'N' || identifier[0] == 'S')
-		&& identifier[1] == 'O' && identifier[2] == 0)
+		&& identifier[1] == 'O' && identifier[2] == 0
+		&& !already_found(identifier[0]))
 		return (1);
-	else if (identifier[0] == 'W' && identifier[1] == 'E' && identifier[2] == 0)
+	else if (identifier[0] == 'W' && identifier[1] == 'E' 
+		&& identifier[2] == 0 && !already_found(identifier[0]))
 		return (1);
-	else if (identifier[0] == 'E' && identifier[1] == 'A' && identifier[2] == 0)
+	else if (identifier[0] == 'E' && identifier[1] == 'A'
+		&& identifier[2] == 0 && !already_found(identifier[0]))
 		return (1);
 	print_error(IDENTIFIER_ERROR);
 	return (0);
@@ -83,7 +87,9 @@ int	add_scene_data(char *line, t_map *file_data)
 		if (!valid_identifier(array[0])
 			|| !store_data(array[0][0], array[1], file_data))
 			return (error_found(array));
-		file_data->parse_checklist += 1;
+		//file_data->parse_checklist += 1;
+		//Change this, what you must do is update exactly integer from
+		//checklist array
 		free_strings_array(array);
 	}
 	return (1);
