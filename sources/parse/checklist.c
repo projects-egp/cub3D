21f064bd39ded155c3a993d1af57480e915a9bfa
Enrique Gil Piqueras <enrgil-p@c2r10s2.42madrid.com>
2026-05-08 17:53:44 +0200

    Forgot to increase value in a loop

--- sources/parse/checklist.c
+++ sources/parse/checklist.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 12:47:02 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/05/08 17:38:02 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/05/08 17:52:27 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,9 @@ int	checklist(t_map *file_data)
 	result = 0;
 	i = 0;
 	while (i < CHECKLIST_SIZE)
+	{
 		result += file_data->parse_checklist[i];
+		++i;
+	}
 	return (result);
 }
