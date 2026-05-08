99fb512f9ee9aed82a263b172127cc643ff6581d
Enrique Gil Piqueras <enrgil-p@c2r10s2.42madrid.com>
2026-05-08 18:01:41 +0200

    Changed returned values from check_if_already_found()

--- sources/parse/checklist.c
+++ sources/parse/checklist.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 12:47:02 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/05/08 17:52:27 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/05/08 18:01:04 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static int	update_checklist(t_map *file_data, int position)
 {
 	file_data->parse_checklist[position] = 1;
-	return (0);
+	return (1);
 }
 
 int	check_if_already_found(char identifier, t_map *file_data)
@@ -33,7 +33,7 @@ int	check_if_already_found(char identifier, t_map *file_data)
 	else if (identifier == 'F' && !file_data->parse_checklist[FOUND_F])
 		return (update_checklist(file_data, FOUND_F));
 	print_error(DUPLICATED_IDENTIFIER);
-	return (1);
+	return (0);
 }
 
 int	checklist(t_map *file_data)
