c0d40256106c3bde5a6cf6a098bfcf11d72c0922
Enrique Gil Piqueras <enrgil-p@c3r11s4.42madrid.com>
2026-05-09 17:43:45 +0200

    destroy_mlx no removes images too

--- sources/clean/destroy_mlx.c
+++ sources/clean/destroy_mlx.c
@@ -6,17 +6,24 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 15:03:42 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/05/09 17:19:04 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/05/09 17:39:02 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "general.h"
 
+static void	remove_images(t_mlx *mlx)
+{
+	if (mlx->img.img_ptr)
+		mlx_destroy_image(mlx->mlx_ptr, mlx->img.img_ptr);
+	return ;
+}
+
 void	destroy_mlx_requested(t_mlx *mlx)
 {
 	if (mlx->win_ptr)
 		mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
-	//remove images ???
+	remove_images(mlx);
 	if (mlx->mlx_ptr)
 	{
 		mlx_destroy_display(mlx->mlx_ptr);
