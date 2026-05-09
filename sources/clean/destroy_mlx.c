f870d3c602931edcb7acbe5456e06cfd0afe0979
Enrique Gil Piqueras <enrgil-p@c3r11s4.42madrid.com>
2026-05-09 17:20:16 +0200

    It's necessary a free, besides mlx_destroy_display

--- sources/clean/destroy_mlx.c
+++ sources/clean/destroy_mlx.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 15:03:42 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/05/09 15:09:45 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/05/09 17:19:04 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	destroy_mlx_requested(t_mlx *mlx)
 	if (mlx->mlx_ptr)
 	{
 		mlx_destroy_display(mlx->mlx_ptr);
-		//free(mlx->mlx_ptr);//test if works without this
+		free(mlx->mlx_ptr);
 	}
 	return ;
 }
