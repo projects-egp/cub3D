8e6453be94cb4646ce3905fe6aeee26ea4f61806
Enrique Gil Piqueras <enrgil-p@c2r7s1.42madrid.com>
2026-05-11 12:11:20 +0200

    Testing map

--- sources/window_mlx_management/background.c
+++ sources/window_mlx_management/background.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 10:41:33 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/05/11 11:38:48 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/05/11 12:10:23 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 uint32_t	get_color_value(int *decimal_value)
 {
-	uint32	result;
+	uint32_t	result;
 
 	result = decimal_value[RED] << 16 | decimal_value[GREEN] << 8
 		| decimal_value[BLUE];
