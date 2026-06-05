ed02bb4f2581d7cafe223615fac898e7156128bc
Enrique Gil Piqueras <enrgil-p@c3r12s2.42madrid.com>
2026-06-05 11:50:05 +0200

    Removed enrgil-p first approach to key_pressed(). key_event will store functions for key_event

--- sources/window_mlx_management/key_event.c
+++ sources/window_mlx_management/key_event.c
@@ -6,15 +6,10 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 10:08:46 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/05/11 10:13:17 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/06/05 11:48:55 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "general.h"
 
-int	key_pressed(int key, t_mlx *mlx)
-{
-	if (key == ESC)
-		close_program(mlx, SUCCESS_CLOSE);
-	return (0);
-}
+
