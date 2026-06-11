58ecd1d1d9f1929de09290f14e1466cbbfb6c765
Enrique Gil Piqueras <enrgil-p@c3r12s6.42madrid.com>
2026-06-11 18:09:06 +0200

    Compiles and works, but texture is not properly printed

--- sources/window_mlx_management/render_3d_scene.c
+++ sources/window_mlx_management/render_3d_scene.c
@@ -6,19 +6,52 @@
 /*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/24 19:41:59 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/06/07 14:54:00 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/06/11 18:04:17 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "general.h"
 
-static void	draw_vertical_line(t_mlx *mlx, int x, double distance, int color)
+static double	get_texture_x(double wall_x, t_img texture, int side,
+		double dx, double dy)
+{
+	double	texture_x;
+
+	texture_x = wall_x * texture.width;
+	if ((side == 1  && dy > 0) || (side == 0 && dx < 0))
+		texture_x = texture.width - texture_x - 1;
+	return (texture_x);
+}
+static t_img	get_texture(t_mlx *mlx, int side, double dx, double dy)
+{
+	if (side == 1) // Impacto en Eje Y (Paredes Norte o Sur)
+	{
+		if (dy > 0)
+			return (mlx->textures[NORTH_PATH]); // Rayo baja: golpea cara NORTE (Rojo)
+		return (mlx->textures[SOUTH_PATH]);// Rayo sube: golpea cara SUR (Amarillo)
+	}
+	else // Impacto en Eje X (Paredes Este u Oeste)
+	{
+		if (dx > 0)
+			return (mlx->textures[WEST_PATH]); // Rayo va derecha: golpea cara OESTE (Verde)
+		return (mlx->textures[EAST_PATH]);// Rayo va izquierda: golpea cara ESTE (Azul)
+	}
+}
+
+static void	draw_vertical_line(t_mlx *mlx, int x, int side, double distance,
+		double dir[2], double wall_x) 
 {
 	int	wall_height;
 	int	y;
 	int	start;
 	int	end;
+	t_img	texture;
+	double	texture_x;
+	double	texture_y;
+	int	color;
 
+	texture = get_texture(mlx, side, dir[X_POS], dir[Y_POS]);
+	texture_x = get_texture_x(wall_x, texture, side, dir[X_POS], dir[Y_POS]);
 	wall_height = (int)(HEIGHT / (distance + 0.0001));
 	start = (HEIGHT / 2) - (wall_height / 2);
 	end = (HEIGHT / 2) + (wall_height / 2);
@@ -29,25 +62,26 @@ static void	draw_vertical_line(t_mlx *mlx, int x, double distance, int color)
 	y = start;
 	while (y < end)
 	{
+		texture_y = (int)((y - start)
+				* ((double)texture.height * wall_height));
+		color = texture.bits_per_pixel * texture_y * texture.width + texture_x;
 		my_pixel_put(&mlx->img, x, y, color);
 		y++;
 	}
 }
 
-static int	get_wall_color(int side, double dx, double dy)
+static double	hit_position(t_mlx *mlx, double distance, double dir[2], int side)
 {
+	double	wall_x;
+
+	wall_x = 0;
 	if (side == 1) // Impacto en Eje Y (Paredes Norte o Sur)
-	{
-		if (dy > 0)
-			return (0xFF0000); // Rayo baja: golpea cara NORTE (Rojo)
-		return (0xFFFF00);// Rayo sube: golpea cara SUR (Amarillo)
-	}
-	else // Impacto en Eje X (Paredes Este u Oeste)
-	{
-		if (dx > 0)
-			return (0x00FF00); // Rayo va derecha: golpea cara OESTE (Verde)
-		return (0x0000FF);// Rayo va izquierda: golpea cara ESTE (Azul)
-	}
+		wall_x = mlx->map_data->player[Y_POS] + distance * dir[X_POS];
+	else
+		wall_x = mlx->map_data->player[X_POS] + distance * dir[Y_POS];
+	wall_x /= 64.0;
+	wall_x -= floor(wall_x);
+	return (wall_x);
 }
 
 /* STEPS
@@ -64,6 +98,7 @@ void	render_3d_scene(t_mlx *mlx)
 	double	distance;
 	int		side;
 	double	dir[POSITION];
+	double	wall_hit_position;
 
 	x = 0;
 	while (x < WIDTH)
@@ -74,9 +109,9 @@ void	render_3d_scene(t_mlx *mlx)
 		dir[X_POS] = cos(ray_angle);
 		dir[Y_POS] = sin(ray_angle);
 		distance = throw_ray(mlx, &side, dir[X_POS], dir[Y_POS]);
+		wall_hit_position = hit_position(mlx, distance, dir, side);
 		distance *= cos(ray_angle - mlx->map_data->player_angle);
-		draw_vertical_line(mlx, x, distance,
-			get_wall_color(side, dir[X_POS], dir[Y_POS]));
+		draw_vertical_line(mlx, x, side, distance, dir, wall_hit_position);	
 		++x;
 	}
 }
