e00d3528af1be82877aadc330dd4a5622f4e532c
Enrique Gil Piqueras <enrgil-p@c1r12s1.42madrid.com>
2026-06-26 11:50:25 +0200

    key_events.c works now only with X11 keys, calling <X11/keysym.h>

--- linux/include/general.h
+++ linux/include/general.h
@@ -6,7 +6,7 @@
 /*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 17:32:53 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/06/09 10:21:26 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/06/26 11:19:06 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 # include "clean.h"
 # include <fcntl.h>
 # include <stdio.h>
+# include <X11/keysym.h>
 # include <stdint.h>/*For uint_32, used to store colors. This integer
 		      guarantees that integer is unsigned and has 4 bytes.
 		      Needed because we use 24 bits to store color*/
--- linux/sources/window_mlx_management/key_events.c
+++ linux/sources/window_mlx_management/key_events.c
@@ -6,44 +6,46 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 10:08:46 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/06/05 21:10:54 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/06/26 11:42:44 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "general.h"
 
+//Both functions of this file uses <X11/keysym.h> to identify keys, as long as
+//program is expected to be executed with X11 at Ubuntu Linux OS
 int	key_pressed(int key, t_mlx *mlx)
 {
-	if (key == 53 || key == 65307)
+	if (key == XK_Escape)
 		close_program(mlx, SUCCESS_CLOSE);
-	if (key == 13 || key == 119)
+	if (key == XK_w)
 		mlx->key[W_KEY] = 1;
-	else if (key == 1 || key == 115)
+	else if (key == XK_s)
 		mlx->key[S_KEY] = 1;
-	else if (key == 0 || key == 97)
+	else if (key == XK_a)
 		mlx->key[A_KEY] = 1;
-	else if (key == 2 || key == 100)
+	else if (key == XK_d)
 		mlx->key[D_KEY] = 1;
-	else if (key == 123 || key == 65361)
+	else if (key == XK_Left)
 		mlx->key[LEFT_KEY] = 1;
-	else if (key == 124 || key == 65363)
+	else if (key == XK_Right)
 		mlx->key[RIGHT_KEY] = 1;
 	return (0);
 }
 
 int	key_released(int key, t_mlx *mlx)
 {
-	if (key == 13 || key == 119)
+	if (key == XK_w)
 		mlx->key[W_KEY] = 0;
-	else if (key == 1 || key == 115)
+	else if (key == XK_s)
 		mlx->key[S_KEY] = 0;
-	else if (key == 0 || key == 97)
+	else if (key == XK_a)
 		mlx->key[A_KEY] = 0;
-	else if (key == 2 || key == 100)
+	else if (key == XK_d)
 		mlx->key[D_KEY] = 0;
-	else if (key == 123 || key == 65361)
+	else if (key == XK_Left)
 		mlx->key[LEFT_KEY] = 0;
-	else if (key == 124 || key == 65363)
+	else if (key == XK_Right)
 		mlx->key[RIGHT_KEY] = 0;
 	return (0);
 }
